aaa88c26b9ff623574bcd0f8bc8171aedc74777c
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2023-09-30 14:49:14 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiFuncional.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFuncional.Data;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Produto> Produtos{ get; set; }
+}
