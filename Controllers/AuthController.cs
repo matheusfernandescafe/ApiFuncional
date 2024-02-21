1a86ca8b27fbe9e152702ae9e1235df0360fb7a0
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2024-02-20 22:21:51 -0300

    feat(AuthController): Modificado a geração de JWT para atender a roles

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace ApiFuncional.Controllers;
@@ -43,7 +44,7 @@ public class AuthController : ControllerBase
         if (result.Succeeded)
         {
             await _signInManager.SignInAsync(user, false);
-            return Ok(GerarJwt());
+            return Ok(await GerarJwt(user.Email));
         }
 
         return Problem("Falha ao registrar o usuário");
@@ -59,19 +60,33 @@ public class AuthController : ControllerBase
 
         if (result.Succeeded)
         {
-            return Ok(GerarJwt());
+            return Ok(await GerarJwt(loginUser.Email));
         }
 
         return Problem("Usuário ou senha incorretos");
     }
 
-    private string GerarJwt()
+    private async Task<string> GerarJwt(string email)
     {
+        var user = await _userManager.FindByNameAsync(email);
+        var roles = await _userManager.GetRolesAsync(user);
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.UserName)
+        };
+
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(_jwtSettings.Segredo);
 
         var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
         {
+            Subject = new ClaimsIdentity(claims),
             Issuer = _jwtSettings.Emissor,
             Audience = _jwtSettings.Audiencia,
             Expires = DateTime.UtcNow.AddHours(_jwtSettings.ExpiracaoHoras),
